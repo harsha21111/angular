ec6e1fda585e526bba2ceb05c6e856be5466c1dd
harshavardhan <harshavardan2k17@gmail.com>
2019-09-16 17:31:09 +0530

    data binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,16 +7,28 @@ import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { FlexLayoutModule } from '@angular/flex-layout';
-
+import { DishService } from './services/dish.service';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
+    DishdetailComponent,
+    AboutComponent,
+    HomeComponent,
+    ContactComponent,
+    HeaderComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
@@ -27,9 +39,12 @@ import { MenuComponent } from './menu/menu.component';
     MatCardModule,
     MatButtonModule,
     FlexLayoutModule,
+    AppRoutingModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    DishService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
